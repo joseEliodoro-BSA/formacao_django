a476971219c84d07bab4477e0bbc50c467d03bd8
Jose Eliodoro <jose.santana@bsatech.io>
2025-10-13 17:35:32 -0300

    feat: adiciona caminho para páginas

--- galeria/views.py
+++ galeria/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "galeria/index.html")
 
-def index2(request):
-    return HttpResponse("<h1>Alura Space-2</h1>")
-
-def index3(request):
-    return HttpResponse("<h1>Alura Space-3</h1>")
+def imagem(request):
+    return render(request, "galeria/imagem.html")
