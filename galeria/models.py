dafd6c16bf4b35820ab4711b385cda462af1a1b0
Jose Eliodoro <jose.santana@bsatech.io>
2025-10-14 17:55:26 -0300

    feat: modelo entidade fotografia

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Fotografia(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField( null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return "{"+f'"nome": {self.nome}, "legenda": {self.legenda}, "descricao": `{self.descricao}, "foto": {self.foto}'+"}"
