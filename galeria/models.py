5e7ecdde6696a40f7993e30aef4436a093f04874
Jose Eliodoro <jose.santana@bsatech.io>
2025-10-15 15:11:55 -0300

    feat: cria novos campos para para entidade

--- galeria/models.py
+++ galeria/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Fotografia(models.Model):
+    OPCAO_CATEGORIA = [
+        ("NEBULOSA", "Nebulosa"),
+        ("GALÁXIA", "Galáxia"),
+        ("PLANETA", "Planeta"),
+        ("ESTRELA", "Estrela"),
+    ]
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
-    descricao = models.TextField( null=False, blank=False)
+    descricao = models.TextField( null=False, blank=False, default="")
+    categoria = models.CharField(max_length=100, choices=OPCAO_CATEGORIA)
     foto = models.CharField(max_length=100, null=False, blank=False)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
         return "{"+f'"nome": {self.nome}, "legenda": {self.legenda}'+"}"
