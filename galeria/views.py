9ca08622bbd8ad4d24aa6cc2b2d01773c494cc24
Jose Eliodoro <jose.santana@bsatech.io>
2025-10-15 17:04:18 -0300

    feat: implementa rota para buscar card

--- galeria/views.py
+++ galeria/views.py
@@ -10,3 +10,14 @@ def index(request):
 def imagem(request, foto_id: int):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, "galeria/imagem.html", {"fotografia": fotografia })
+
+def buscar(request):
+    fotografia = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
+
+    if "buscar" in request.GET:
+        nome_buscar = request.GET['buscar']
+        if nome_buscar:
+            fotografia = fotografia.filter(nome__icontains=nome_buscar)
+
+    print(fotografia)
+    return render(request, "galeria/buscar.html", {"cards": fotografia})
