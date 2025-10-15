3658b96a00db2b1ca6bcb837c578da844f942787
Jose Eliodoro <jose.santana@bsatech.io>
2025-10-15 15:13:40 -0300

    feat: adiciona novos campos de listas no django admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListandoFotografia(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'legenda')
+    list_display = ('id', 'nome', 'legenda', 'publicada')
     list_display_links = ('id', 'nome')
+    list_filter = ("categoria",)
+    list_per_page = 10
+    list_editable = ("publicada", )
+
     search_fields = ("nome","legenda")
 
 admin.site.register(Fotografia, ListandoFotografia)
