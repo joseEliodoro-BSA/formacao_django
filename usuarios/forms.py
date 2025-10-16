f1f670d0dc107970b7b0ad0489cde9543fea2427
Jose Eliodoro <jose.santana@bsatech.io>
2025-10-16 14:54:46 -0300

    feat: implementa formulários de login e cadastro

---
+++ usuarios/forms.py
@@ -0,0 +1,76 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    nome_login = forms.CharField(
+       label= "Nome do Login", 
+       required= True,
+       max_length= 100,
+       widget= forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': "Digite seu nome aqui..."
+            }
+        ),
+    )
+
+    senha = forms.CharField(
+        label= "Senha", 
+        required= True,
+        max_length= 70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite a sua senha',
+            }
+        ),
+    )
+    t = forms.TextInput()
+
+## Formulário de cadastro
+class CadastroForm(forms.Form):
+    nome_cadastro = forms.CharField(
+       label= "Nome do Cadastro", 
+       required= True,
+       max_length= 100,
+       widget= forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': "Digite seu nome aqui..."
+            }
+        ),
+    )
+    nome_email = forms.EmailField(
+       label= "Email", 
+       required= True,
+       max_length= 100,
+       widget= forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': "Digite seu email"
+            }
+        ),
+    )
+
+    senha_1 = forms.CharField(
+        label= "Senha", 
+        required= True,
+        max_length= 70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite a sua senha',
+            }
+        ),
+    )
+
+    senha_2 = forms.CharField(
+        label= "Confirme sua senha", 
+        required= True,
+        max_length= 70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite a sua senha novamente',
+            }
+        ),
+    )
