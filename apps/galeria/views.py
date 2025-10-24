ec68b65c28643a3e2ec54e23ecb7ce435043a8aa
Jose Eliodoro <jose.santana@bsatech.io>
2025-10-24 12:20:26 -0300

    faet: adiciona lógica de cadastro da nova imagem

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -5,6 +5,7 @@ from apps.galeria.forms import FotografiaForms
 from apps.galeria.models import Fotografia
 from django.contrib import messages
 
+
 def index(request):
     if not request.user.is_authenticated:
         messages.error(request, "Usuário não logado")
@@ -34,7 +35,20 @@ def buscar(request):
     return render(request, "galeria/buscar.html", {"cards": fotografia})
 
 def nova_imagem(request):
-    form = FotografiaForms()
+    
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuário não logado")
+        return redirect("login")
+    
+    form = FotografiaForms
+    if request.method == "POST":
+        form = FotografiaForms(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Nova fotografia cadastrada!')
+            return redirect('index')
+
     return render(request, "galeria/nova_imagem.html", {"form": form})
 
 def editar_imagem(request):
